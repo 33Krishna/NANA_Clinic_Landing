b7c16fa12dada033a2b5c5ac191322008379ab88
Franky1250 <tpatra406@gmail.com>
2025-10-05 00:23:39 +0530

    fix image of authorised brand for better view

--- frontend/src/components/BrandSlider.tsx
+++ frontend/src/components/BrandSlider.tsx
@@ -4,7 +4,7 @@ import Phonak from '/images/Brands/Phonak.jpg'
 import ReSound from '/images/Brands/ReSound.jpg'
 import Unitron from '/images/Brands/Unitron.jpg'
 import Widex from '/images/Brands/Widex.jpg'
-import AuthorisedPartner from '/images/Brands/AuthorisedPartner.jpg'
+import AuthorisedPartner from '/images/Brands/AuthorisedPartner.png'
 
 
 const BrandSlider = () => {
@@ -17,7 +17,7 @@ const BrandSlider = () => {
         </h2>
         
         {/* Authorised Partner Image */}
-        <div className="text-center mb-8 reveal" style={{ transitionDelay: "200ms" }}>
+        <div className="text-center reveal" style={{ transitionDelay: "200ms" }}>
           <img
             src={AuthorisedPartner}
             alt="Authorised Partner"
