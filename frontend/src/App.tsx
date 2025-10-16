040c1896285dc9537b4b18825f2e1e9fd1a63a5a
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-10-16 20:30:55 +0530

    facebook pixel added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import FacebookPixel from "./components/FacebookPixel";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <FacebookPixel />
       <Toaster />
       <Sonner />
       <BrowserRouter>
